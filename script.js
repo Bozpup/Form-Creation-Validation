f2c7fb7cc26d863ab6684102c46bc16f2ebbc7d9
Fikayo Banjo <banjofikayo@gmail.com>
2024-10-13 19:41:23 +0100

    form validation

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code to manipulate DOM or initialize your script
+  console.log("The DOM is fully loaded");
+});
+
+const form = document.getElementById("registration-form");
+const feedbackDiv = document.getElementById("form-feedback");
+
+form.addEventListener("submit", (event) => {
+  let isValid = true;
+  let messages = [];
+  event.preventDefault();
+  const usernameInput = document.getElementById("username").value.trim();
+  const emailInput = document.getElementById("email").value.trim();
+  const passwordInput = document.getElementById("password").value.trim();
+
+  if (usernameInput.length < 3) {
+    isValid = false;
+    messages.push("Username less than 3");
+    console.log(messages);
+  }
+  if (!emailInput.includes("@")) {
+    isValid = false;
+    messages.push("@ is missing");
+    console.log(messages);
+  }
+  if (passwordInput.length <= 8) {
+    isValid = false;
+    messages.push("Password not long enough");
+    console.log(messages);
+  }
+
+  feedbackDiv.style.display = "block";
+  if (isValid == true) {
+    feedbackDiv.textContent = "Registration Successful";
+    feedbackDiv.style.color = "#28a745";
+  } else {
+    feedbackDiv.innerHTML = messages.join("<br>"); // Display error messages with line breaks
+    feedbackDiv.style.color = "#dc3545";
+  }
+
+  console.log(usernameInput, emailInput, passwordInput);
+
+  if (!isValid) {
+    feedbackDiv.textContent = "Not valid";
+    console.log(feedbackDiv.textContent); // Display error messages in the feedbackDiv
+  } else {
+    form.submit(); // Submit the form only if it is valid
+  }
+});
